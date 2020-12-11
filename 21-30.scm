a6689b48798e5b6c623e29f5c776195a2523e7cc
Sergey Ivanov <powah.serge@gmail.com>
2020-12-11 17:02:24 -0500

    up to problem 25, partitioning is magic

---
+++ 21-30.scm
@@ -0,0 +1,35 @@
+;; 21 - Insert an element at a given position into a list
+; I never know whether to make these input-robust or if it's more helpful to break if not used correctly
+(define (insert-at o l n)
+  (if (or (<= n 1) (null? l)) (cons o l)
+   (cons (car l) (insert-at o (cdr l) (- n 1)))))
+
+;; 22 - Create a list containing all integers within a given range
+(define (range a b)
+  (if (> a b) '()
+   (cons a (range (+ a 1) b))))
+
+;; 23 - Extract a given number of randomly selected elements from a list
+
+;; helper fn to get kth element and move it to start of list
+(define (get-k-partition l k ctr)
+  (if (null? l) '()
+  (if (< ctr k)
+    (let ((fst (car l))
+          (rst (get-k-partition (cdr l) k (+ ctr 1))))
+    (cons (car rst) (cons fst (cdr rst))))
+    l)))
+
+(define (rnd-select l n)
+  (if (or (= n 0) (null? l)) '()
+   (let ((part (get-k-partition l (random (+ (length l) 1)) 1)))
+    (cons (car part) (rnd-select (cdr part) (- n 1))))))
+
+;; 24 - Lotto: Draw N different random numbers from the set 1..M
+(define (lotto-select n M)
+  (rnd-select (range 1 M) n))
+
+;; 25 - Generate a random permutation of the elements of a list
+(define (rnd-permu l)
+  (rnd-select l (length l)))
+   
